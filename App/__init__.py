65fbfe726ec0020a8604af5e0b61a6515df8bca5
weiawesome <70155287+weiawesome@users.noreply.github.com>
2022-10-21 00:50:17 +0800

    Add files via upload

---
+++ App/__init__.py
@@ -0,0 +1,48 @@
+from flask import *
+import time,json
+import datetime
+
+app=Flask(__name__)
+chiness_weeksate=['零','一','二','三','四','五','六','日']
+@app.route('/',methods=['GET'])
+def home_page():
+    data_set={'Page':'Home','Messege':'Sucess loaded the home page','Timestamp':time.time()}
+    json_dump=json.dumps(data_set)
+    return json_dump
+
+@app.route('/user/',methods=['GET'])
+def request_page():
+    user_query=str(request.args.get('user'))
+    # print(user_query)
+    data_set={'Page':'Request','Messege':f'Sucess got the request for {user_query}','Timestamp':time.time()}
+    json_dump=json.dumps(data_set)
+    return json_dump
+
+@app.route('/test/',methods=['POST'])
+def test_page():
+    input=request.get_json()
+    start=input['start']
+    end=input['end']
+    oneway_return=input['oneway_return']
+    gotime=input['gotime']
+    returntime=input['returntime']
+    traintype=input['traintype']
+    people=input['people']
+    prefer=input['prefer']
+    data=[start,end,oneway_return,gotime,returntime,traintype,people,prefer]
+    print(data)
+    return jsonify({'a':'ok!'})
+
+@app.route('/time/',methods=['GET'])
+def time_page():
+    t=datetime.datetime.today()
+    YMD=str(t.year)+'/'+str(t.month)+'/'+str(t.day)
+    HM=str(t.hour)+':'+str(t.minute)
+    W=t.isoweekday()
+    data_set={'YMD':YMD,'HM':HM,'W':W}
+    json_dump=json.dumps(data_set)
+    return json_dump
+
+
+if __name__=='__main__':
+    app.run(port=7777,debug=True)
---
+++ run.py
@@ -0,0 +1,8 @@
+from App import App
+
+@app.route('/')
+def index():
+    return 'Flask API started'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000, debug=False)
