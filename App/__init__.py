04fbd1eb16f19ae4a73e3897b0382d78fedc3df2
weiawesome <70155287+weiawesome@users.noreply.github.com>
2022-10-28 18:05:32 +0800

    Update __init__.py

--- App/__init__.py
+++ App/__init__.py
@@ -28,7 +28,7 @@ def test_page():
     data=[start,end,oneway_return,gotime,returntime,traintype,people,prefer]
     
     price=[]
-    for i in people.split(','):
+    for i in (people.split(',')):
         price.append(int(i)*100))
     num=random.randint(0, 100)
     datas=[]
