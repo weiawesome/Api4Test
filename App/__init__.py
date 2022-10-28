8e9402156387098992a7b55532af9d5380886c46
weiawesome <70155287+weiawesome@users.noreply.github.com>
2022-10-28 18:13:21 +0800

    Update __init__.py

--- App/__init__.py
+++ App/__init__.py
@@ -29,7 +29,7 @@ def test_page():
     
     price=[]
     for i in (people.split(',')):
-        price.append(int(i)*100))
+        price.append(int(i)*100)
     
     num=random.randint(0, 100)
     datas=[]
