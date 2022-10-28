9897626db00279408bd0f8a08130ae146d5af659
weiawesome <70155287+weiawesome@users.noreply.github.com>
2022-10-28 16:36:54 +0800

    Update __init__.py

--- App/__init__.py
+++ App/__init__.py
@@ -92,7 +92,6 @@ def book_page():
     BackArriveTime=input['BackArriveTime']
     Type=input['Type']
 
-    Tickets=int(Tickets)
     seat=[]
     n = [ '1', '2', '3', '4', '5', '6', '7', '8', '9']
     t=['A','B','C','D','E']
