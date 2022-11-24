68a6d78364fbcc5d74b6ecda7169e53816517a7f
weiawesome <70155287+weiawesome@users.noreply.github.com>
2022-11-24 23:52:06 +0800

    Update __init__.py

--- App/__init__.py
+++ App/__init__.py
@@ -9,8 +9,8 @@ app = Flask(__name__)
 HOST = '140.136.151.128'
 PORT = 10001
 
-def GetStationsBy(item):
 
+def GetStationsBy(item):
     ################################################
     # Split the Data
     ################################################
@@ -22,8 +22,10 @@ def GetStationsBy(item):
             result[i] = result[i][:5]
     return result
 
+
 def TimeCmp(item):
     return int(item["StartTime"][:2]) * 60 + int(item["StartTime"][3:5]) * 1
+
 
 def GetDataFromSocket(commands):
     print(commands)
@@ -49,14 +51,13 @@ def GetDataFromSocket(commands):
 
 @app.route('/GetTrains/', methods=['POST'])
 def GetTrains_page():
-
     ################################################
     # Get Input Datas
     ################################################
     input = request.get_json()
     StartStation = input['StartStation']
     ArriveStation = input['ArriveStation']
-    OnewayReturn= input['OnewayReturn']
+    OnewayReturn = input['OnewayReturn']
     StartTime = input['StartTime']
     BackStartTime = input['BackStartTime']
     Type = input['Type']
@@ -66,8 +67,8 @@ def GetTrains_page():
     ################################################
     # Set Data sent to Socket
     ################################################
-    State=''
-    
+    State = ''
+
     if (OnewayReturn != 'true'):
         State = 'True'
     else:
@@ -95,25 +96,25 @@ def GetTrains_page():
     BackDatas = []
 
     for i in Result['Datas']:
-        StationsBy=GetStationsBy(i['StationsBy'])
+        StationsBy = GetStationsBy(i['StationsBy'])
         Datas.append(
             {
-                'StartTime': i['StartTime'][:5], 
-                'ArriveTime': i['ArriveTime'][:5], 
+                'StartTime': i['StartTime'][:5],
+                'ArriveTime': i['ArriveTime'][:5],
                 'TotalTime': '',
                 'Order': i['Order'],
                 'StationsBy': StationsBy
             }
         )
-        
+
     if (State == 'False'):
         sorted(Result['BackDatas'], key=TimeCmp)
         for i in Result['BackDatas']:
             StationsBy = GetStationsBy(i['StationsBy'])
             BackDatas.append(
                 {
-                    'StartTime': i['StartTime'][:5], 
-                    'ArriveTime': i['ArriveTime'][:5], 
+                    'StartTime': i['StartTime'][:5],
+                    'ArriveTime': i['ArriveTime'][:5],
                     'TotalTime': '',
                     'Order': i['Order'],
                     'StationsBy': StationsBy
@@ -153,25 +154,60 @@ def edit_page():
     Type = input['Type']
     Tickets = input['Tickets']
 
-    Fees = []
-    price = []
-    for i in (Tickets.split(',')):
-        price.append(int(i) * 100)
-        Fees.append(int(i) * 10)
 
-    num = random.randint(0, 100)
-    datas = []
-    for i in range(num):
-        datas.append({'StartTime': '10:34', 'ArriveTime': '11:40', 'TotalTime': '1時 06分', 'Order': '616',
-                      'StationsBy': ['21:30', '21:41', '21:50', '22:05', '22:17', '', '22:43', '', '', '23:09', '23:28',
-                                     '23:40']})
-    num = random.randint(0, 100)
-    retrundata = {
-        'Price': price,
-        'Datas': datas,
-        'Fees': Fees
+    Command = {
+        "CommandType": "GetTrains",
+        "StartStation": StartStation,
+        "ArriveStation": ArriveStation,
+        "OneWayReturn": 'False',
+        "StartDate": StartTime[0:10],
+        "StartTime": StartTime[15:],
+        "BackStartDate": '',
+        "BackStartTime": '',
+        "Type": Type,
+        "Prefer": '無偏好'
     }
-    Json_Dump = json.dumps(retrundata)
+    Result = GetDataFromSocket(Command)
+    sorted(Result['Datas'], key=TimeCmp)
+
+    ################################################
+    # Set the Datas
+    ################################################
+    Datas = []
+
+    for i in Result['Datas']:
+        StationsBy = GetStationsBy(i['StationsBy'])
+        Datas.append(
+            {
+                'StartTime': i['StartTime'][:5],
+                'ArriveTime': i['ArriveTime'][:5],
+                'TotalTime': '',
+                'Order': i['Order'],
+                'StationsBy': StationsBy
+            }
+        )
+
+    ################################################
+    # Set the Price
+    ################################################
+    Price = (Result['TicketPrice']).split(',')
+    Price[2], Price[3], Price[4] = Price[1], Price[1], int(Price[0]) * 0.88
+    Tickets = Tickets.split(',')
+    for i in range(len(Tickets)):
+        Price[i] = int(Price[i]) * int(Tickets[i])
+        if (Tickets == 'False'):
+            Price[i] *= 2
+
+    ################################################
+    # Set the Return Datas and to Json type
+    ################################################
+    RetrunDatas = {
+        'Price': Price,
+        'Datas': Datas,
+        'Fees':[]
+    }
+    Json_Dump = json.dumps(RetrunDatas)
+
     return Json_Dump
 
 
@@ -181,32 +217,44 @@ def getlose_page():
     ID = input['ID']
     BookID = input['BookID']
 
+    j={
+        "CommandType": "FindLose",
+        "ID": ID,
+        "BookID": BookID
+    }
+    a=GetDataFromSocket(j)
+    ts=str(a['Tickets']).split(',')
+    ps=str(a['Prices']).split(',')
+    for i in range(len(ts)):
+        ts[i]=int(ts[i])
+    for i in range(len(ps)):
+        ps[i]=int(ps[i])
     retrundata = {
-        'Status': 'True',
-        'StartStation': '左營',
-        'ArriveStation': '板橋',
-        'OnewayReturn': 'True',
-        'Type': '商務車廂',
+        'Status': a['Status'],
+        'StartStation': a['Data1']['StartStation'],
+        'ArriveStation': a['Data1']['ArriveStation'],
+        'OnewayReturn': a['OnewayReturn'],
+        'Type': a['Type'],
         'Start': {
-            'Date': '2022/10/13',
-            'StartTime': '10:10',
-            'ArriveTime': '12:00',
-            'TotalTime': '1時5分',
-            'Order': '987',
-            'Seat': [['5車3A'], ['3車4A', '6車9A'], [], [], []],
-            'StationsBy': ['21:30', '21:41', '21:50', '22:05', '22:17', '', '22:43', '', '', '23:09', '23:28', '23:40']
+            'Date': str(a['Data1']['Date'][:10]).replace('-','/'),
+            'StartTime': a['Data1']['StartTime'],
+            'ArriveTime': a['Data1']['ArriveTime'],
+            'TotalTime': '',
+            'Order': a['Data1']['Order'],
+            'Seat': [str((a['Data1']['Seat1']).replace('cabin','車')).split(','), str((a['Data1']['Seat2']).replace('cabin','車')).split(','), str((a['Data1']['Seat3']).replace('cabin','車')).split(','),str((a['Data1']['Seat3']).replace('cabin','車')).split(','),str((a['Data1']['Seat4']).replace('cabin','車')).split(','),str((a['Data1']['Seat5']).replace('cabin','車')).split(',')],
+            'StationsBy': GetStationsBy(a['Data1']['StationsBy'])
         },
-        'Arrive': {
-            'Date': '2022/10/13',
-            'StartTime': '13:00',
-            'ArriveTime': '15:00',
-            'TotalTime': '2時0分',
-            'Order': '987',
-            'Seat': [['5車3A'], ['3車4A', '6車9A'], [], [], []],
-            'StationsBy': ['21:30', '21:41', '21:50', '22:05', '22:17', '', '22:43', '', '', '23:09', '23:28', '23:40']
+        'Arrive': {} if(a['OnewayReturn']=='False') else {
+            'Date': str(a['Data2']['Date'][:10]).replace('-','/'),
+            'StartTime': a['Data2']['StartTime'],
+            'ArriveTime': a['Data2']['ArriveTime'],
+            'TotalTime': '',
+            'Order': a['Data2']['Order'],
+            'Seat': [str((a['Data2']['Seat1']).replace('cabin','車')).split(','), str((a['Data1']['Seat2']).replace('cabin','車')).split(','), str((a['Data1']['Seat3']).replace('cabin','車')).split(','),str((a['Data1']['Seat3']).replace('cabin','車')).split(','),str((a['Data1']['Seat4']).replace('cabin','車')).split(','),str((a['Data1']['Seat5']).replace('cabin','車')).split(',')],
+            'StationsBy': GetStationsBy(a['Data2']['StationsBy'])
         },
-        'Tickets': [1, 2, 0, 0, 0],
-        'Prices': [100, 200, 0, 0, 0],
+        'Tickets': ts,
+        'Prices': ps,
     }
 
     Json_Dump = json.dumps(retrundata)
@@ -215,7 +263,6 @@ def getlose_page():
 
 @app.route('/TimeTable/', methods=['POST'])
 def TimeTable_page():
-
     ################################################
     # Get Input Datas
     ################################################
@@ -248,7 +295,7 @@ def TimeTable_page():
     Datas = []
 
     for i in Result['Datas']:
-        StationsBy=GetStationsBy(i['StationsBy'])
+        StationsBy = GetStationsBy(i['StationsBy'])
         Datas.append(
             {
                 'StartTime': i['StartTime'][:5],
@@ -289,9 +336,13 @@ def pay_page():
 def use_page():
     input = request.get_json()
     BookID = input['BookID']
+    OnewayReturn=input['OnewayReturn']
     Order = input['Order']
     Seat = input['Seat']
+    ArriveOrder = input['ArriveOrder']
+    ArriveSeat = input['ArriveSeat']
     Seat = str(Seat).replace('車', 'cabin')
+    ArriveSeat = str(ArriveSeat).replace('車', 'cabin')
     j = {
         "CommandType": "Use",
         "BookID": BookID,
@@ -299,7 +350,20 @@ def use_page():
         "Seat": Seat
     }
     a = GetDataFromSocket(j)
-    data_set = {'Status': a['UseResult'], 'Out': 'True'}
+    if(OnewayReturn!='true'):
+        data_set = {'Status': a['UseResult'], 'Out': 'True'}
+    else:
+        if(a['UseResult']=='True'):
+            data_set = {'Status': a['UseResult'], 'Out': 'False'}
+        else:
+            j = {
+                "CommandType": "Use",
+                "BookID": BookID,
+                "Order": ArriveOrder,
+                "Seat": ArriveSeat
+            }
+            a = GetDataFromSocket(j)
+            data_set = {'Status': a['UseResult'], 'Out': 'True'}
     Json_Dump = json.dumps(data_set)
     return Json_Dump
 
@@ -333,14 +397,19 @@ def findnow_page():
     StartTime = input['StartTime']
     Order = input['Order']
     ID = input['ID']
-    num = random.randint(1, 5)
+    j={
+      "CommandType": "FindCode",
+      "StartStation": StartStation,
+      "ArriveStation": ArriveStation,
+      "StartDate": StartTime,
+      "Order": Order,
+      "ID": ID
+    }
+    a=GetDataFromSocket(j)
     datas = []
-    for i in range(num):
-        datas.append({'Code': '12345678', 'State': 'False', 'DeadLine': 'False'})
-    num = random.randint(1, 5)
-    for i in range(num):
-        datas.append({'Code': '12345678', 'State': 'True', 'DeadLine': 'False'})
-    data_set = {'Status': 'True', 'Datas': datas}
+    for i in a['Datas']:
+        datas.append({'Code': i['Code'], 'State': i['PayResult']})
+    data_set = {'Status': a['Status'], 'Datas': datas}
     Json_Dump = json.dumps(data_set)
     return Json_Dump
 
@@ -456,28 +525,55 @@ def editnow_page():
     BackArriveTime = input['BackArriveTime']
     Tickets = input['Tickets']
 
-    seat = []
-    backseat = []
-    n = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-    t = ['A', 'B', 'C', 'D', 'E']
-    v = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13']
-    for i in Tickets.split(','):
-        tmp = []
-        for j in range(int(i)):
-            s = ''
-            s = random.choice(n) + '車' + '-' + random.choice(v) + random.choice(t)
-            tmp.append(s)
-        seat.append(tmp)
-    if (BackOrder != 'None'):
-        for i in Tickets.split(','):
-            tmp = []
-            for j in range(int(i)):
-                s = ''
-                s = random.choice(n) + '車' + '-' + random.choice(v) + random.choice(t)
-                tmp.append(s)
-            backseat.append(tmp)
+    j={
+        "CommandType": "Edit",
+        "BookID": BookID,
+        "OneWayReturn": "False" if BackOrder=='None' else 'True',
+        "StartDate": StartDate,
+        "BackDate": BackDate,
+        "Order": Order,
+        "BackOrder": BackOrder
+    }
+    
+    a=GetDataFromSocket(j)
 
-    data_set = {'Status': 'True', 'Seat': seat, 'BackSeat': backseat}
+    seats = []
+    backseats = []
+    seat1 = a['GoSeat1'].split(',')
+    seat2 = a['GoSeat2'].split(',')
+    seat3 = a['GoSeat3'].split(',')
+    seat4 = a['GoSeat4'].split(',')
+    seat5 = a['GoSeat5'].split(',')
+    for i in range(len(seat1)):
+        seat1[i] = seat1[i].replace('cabin', '車')
+    for i in range(len(seat2)):
+        seat2[i] = seat2[i].replace('cabin', '車')
+    for i in range(len(seat3)):
+        seat3[i] = seat3[i].replace('cabin', '車')
+    for i in range(len(seat4)):
+        seat4[i] = seat4[i].replace('cabin', '車')
+    for i in range(len(seat5)):
+        seat5[i] = seat5[i].replace('cabin', '車')
+    seats = [seat1, seat2, seat3, seat4, seat5]
+    if (BackOrder != 'None'):
+        backseat1 = a['BackSeat1'].split(',')
+        backseat2 = a['BackSeat2'].split(',')
+        backseat3 = a['BackSeat3'].split(',')
+        backseat4 = a['BackSeat4'].split(',')
+        backseat5 = a['BackSeat5'].split(',')
+        for i in range(len(backseat1)):
+            backseat1[i] = backseat1[i].replace('cabin', '車')
+        for i in range(len(backseat2)):
+            backseat2[i] = backseat2[i].replace('cabin', '車')
+        for i in range(len(backseat3)):
+            backseat3[i] = backseat3[i].replace('cabin', '車')
+        for i in range(len(backseat4)):
+            backseat4[i] = backseat4[i].replace('cabin', '車')
+        for i in range(len(backseat5)):
+            backseat5[i] = backseat5[i].replace('cabin', '車')
+        backseats = [backseat1, backseat2, backseat3, backseat4, backseat5]
+
+    data_set = {'Status': a['Status'], 'Seat': seats, 'BackSeat': backseats}
     Json_Dump = json.dumps(data_set)
 
     return Json_Dump
